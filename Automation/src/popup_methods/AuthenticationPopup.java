670ea76b896132012ef040f3ed3dae0b3acfbbde
Venugopal V <57706499+venugopal-jpg@users.noreply.github.com>
2026-02-19 11:40:51 +0530

    Update AuthenticationPopup.java

--- Automation/src/popup_methods/AuthenticationPopup.java
+++ Automation/src/popup_methods/AuthenticationPopup.java
@@ -6,9 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AuthenticationPopup {
 public static void main(String[] args) {
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver = new EdgeDriver();
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
 }
 }
+
