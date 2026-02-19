ca2e8e73e1fef3c5b009c05a6d162fca73a648ef
Venugopal V <57706499+venugopal-jpg@users.noreply.github.com>
2026-02-19 11:46:10 +0530

    Update AuthenticationPopup.java

--- Automation/src/popup_methods/AuthenticationPopup.java
+++ Automation/src/popup_methods/AuthenticationPopup.java
@@ -10,7 +10,10 @@ public static void main(String[] args) {
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+
+	WebDriver driver = new ChromeDriver();
 }
 }
+
 
 
